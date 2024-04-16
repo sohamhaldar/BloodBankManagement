374992572b904a8b7295671afc764195738e0651
soham haldar <sohamhaldar25@gmail.com>
2024-04-16 23:23:16 +0530

    changed cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import cookieParser from 'cookie-parser';
 
 
 const app=express();
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'));
